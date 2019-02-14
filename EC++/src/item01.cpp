843008ba0f3bb118e3e341dd91ba102f4f7edf30
NLife91 <rlawhddnr91@gmail.com>
2019-02-14 22:19:49 +0900

    Add item01.

---
+++ EC++/src/item01.cpp
@@ -0,0 +1,42 @@
+/** Item 1: View C++ as a federation of languages. */
+
+#include <stdio.h>
+#include <iostream>
+#include <list>
+
+class TempClass
+{
+public:
+	int GetInteger() { return integer; }
+	void SetInteger(int newInt) { integer = newInt; }
+
+private:
+	int integer;
+};
+
+template<typename T>
+class TemplateClass
+{
+public:
+	T value;
+};
+
+void item01()
+{
+	/** C */
+	printf("Hello World! %d \n", 1);
+
+	/** C++ */ 
+	TempClass temp{ }; 
+	temp.SetInteger(2);
+	std::cout << "Hello World! " << temp.GetInteger() << std::endl;
+
+	/** Template C++ */
+	TemplateClass<TempClass> templateTemp{ };
+	templateTemp.value.SetInteger(3);
+	std::cout << "Hello World! " << templateTemp.value.GetInteger() << std::endl;
+
+	/** STL */
+	std::list<int> tempList{ 4 };
+	std::cout << "Hello World! " << tempList.front() << std::endl;
+}
